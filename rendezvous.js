9f9e095cab8dc0a61db65de7fd5e40bd669280b8
ErivanPereira <34924779+ErivanPereira@users.noreply.github.com>
2019-05-01 21:25:02 -0300

    Update rendezvous.js

--- rendezvous.js
+++ rendezvous.js
@@ -7,6 +7,9 @@ var clients = {};
 
 udp_matchmaker.on('listening', function() {
   var address = udp_matchmaker.address();
+  externalip(function (err, ip) {
+    console.log("IP" + ip); // => 8.8.8.8
+  });
   console.log('# listening [%s:%s]', address.address, address.port);
 });
 
